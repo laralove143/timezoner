4e54192ad20362568195958527ed83905fcf21fc
laralove <laralove143@icloud.com>
2022-12-25 14:19:33 +0300

    Execute a webhook as member

--- src/message.rs
+++ src/message.rs
@@ -102,11 +102,90 @@ impl Context {
             .delete_message(message.channel_id, message.id)
             .await?;
 
-        self.bot
+        self.execute_webhook_as_member(message).await?;
+
+        Ok(())
+    }
+
+    async fn execute_webhook_as_member(&self, message: Message) -> Result<()> {
+        let mut channel_id = message.channel_id;
+        let mut thread_id = None;
+        let channel = self
+            .bot
             .http
-            .create_message(message.channel_id)
-            .content(&message.content)?
+            .channel(message.channel_id)
+            .await?
+            .model()
             .await?;
+        if channel.kind.is_thread() {
+            channel_id = channel.parent_id.ok()?;
+            thread_id = Some(message.channel_id);
+        };
+
+        let webhook = match self
+            .bot
+            .http
+            .channel_webhooks(channel_id)
+            .await?
+            .models()
+            .await?
+            .into_iter()
+            .find(|webhook| webhook.token.is_some())
+        {
+            Some(webhook) => webhook,
+            None => {
+                self.bot
+                    .http
+                    .create_webhook(channel_id, "time sender")?
+                    .await?
+                    .model()
+                    .await?
+            }
+        };
+        let webhook_token = webhook.token.ok()?;
+
+        let mut execute_webhook = self
+            .bot
+            .http
+            .execute_webhook(webhook.id, &webhook_token)
+            .content(&message.content)
+            .map_err(|_| CustomError::MessageTooLong)?
+            .username(
+                message
+                    .member
+                    .as_ref()
+                    .and_then(|member| member.nick.as_ref())
+                    .unwrap_or(&message.author.name),
+            )?;
+
+        if let Some(thread_id) = thread_id {
+            execute_webhook = execute_webhook.thread_id(thread_id);
+        }
+
+        if let Some(avatar_url) = message
+            .member
+            .as_ref()
+            .and_then(|member| member.avatar)
+            .zip(message.guild_id)
+            .map(|(avatar, guild_id)| {
+                format!(
+                    "https://cdn.discordapp.com/guilds/{guild_id}/users/{}/avatar/{}.png",
+                    message.author.id, avatar
+                )
+            })
+            .or_else(|| {
+                message.author.avatar.map(|avatar| {
+                    format!(
+                        "https://cdn.discordapp.com/avatars/{}/{}.png",
+                        message.author.id, avatar
+                    )
+                })
+            })
+        {
+            execute_webhook.avatar_url(&avatar_url).await?;
+        } else {
+            execute_webhook.await?;
+        }
 
         Ok(())
     }
