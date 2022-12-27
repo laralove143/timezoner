680098b6a4f76ed48e1c3e8aac435e7c1b350a92
laralove <laralove143@icloud.com>
2022-12-28 00:02:57 +0300

    Close #13

--- src/interaction/help.rs
+++ src/interaction/help.rs
@@ -1,11 +1,14 @@
 use anyhow::Result;
-use sparkle_convenience::reply::Reply;
+use sparkle_convenience::{prettify::Prettify, reply::Reply};
 use twilight_interactions::command::CreateCommand;
-use twilight_model::channel::message::{embed::EmbedField, Embed};
+use twilight_model::{
+    channel::message::{embed::EmbedField, Embed},
+    guild::Permissions,
+};
 
 use crate::{
     interaction::{CommandIds, InteractionContext},
-    ACCENT_COLOR, BOT_INVITE, SUPPORT_SERVER_INVITE,
+    ACCENT_COLOR, BOT_INVITE, REQUIRED_PERMISSIONS, SUPPORT_SERVER_INVITE,
 };
 
 #[derive(CreateCommand)]
@@ -45,8 +48,8 @@ fn help_embed(command_ids: CommandIds) -> Embed {
                        that server sucks?"
                     .to_owned(),
                 value: format!(
-                    "On mobile you can just hold on the message and click `Copy Text`, on PC, use \
-                     the </copy:{}> command and copy-paste that magic code around",
+                    "On mobile you can just hold on the message and click **Copy Text**, on PC, \
+                     use the </copy:{}> command and copy-paste that magic code around",
                     command_ids.copy
                 ),
                 inline: false,
@@ -66,8 +69,66 @@ fn help_embed(command_ids: CommandIds) -> Embed {
     }
 }
 
+fn missing_permissions_embed(application_name: &str, missing_permissions: Permissions) -> Embed {
+    Embed {
+        color: Some(ACCENT_COLOR),
+        title: Some("Permissions error detected, call tech support!".to_owned()),
+        fields: vec![
+            EmbedField {
+                name: "How to fix??".to_owned(),
+                value: format!(
+                    "Go to **Server Settings -> Roles -> {application_name} -> Permissions** and \
+                     turn on those permissions I listed, if they're already given, read below and \
+                     have fun"
+                ),
+                inline: false,
+            },
+            EmbedField {
+                name: "How to fix (extreme edition)".to_owned(),
+                value: format!(
+                    "Right click the channel you're in right now, click **Edit Channel -> \
+                     Permissions -> Advanced Permissions -> Tiny little + button**, type \
+                     {application_name} and select the text or user, finally put all those \
+                     permissions I listed to a nice looking green tick"
+                ),
+                inline: false,
+            },
+            EmbedField {
+                name: "Missing permissions".to_owned(),
+                value: missing_permissions.prettify(),
+                inline: false,
+            },
+        ],
+        kind: String::new(),
+        author: None,
+        description: None,
+        footer: None,
+        image: None,
+        provider: None,
+        thumbnail: None,
+        timestamp: None,
+        url: None,
+        video: None,
+    }
+}
+
 impl InteractionContext<'_> {
     pub async fn handle_help_command(&self) -> Result<()> {
+        if let Some(app_permissions) = self.interaction.app_permissions {
+            let missing_permissions = REQUIRED_PERMISSIONS - app_permissions;
+
+            if !missing_permissions.is_empty() {
+                self.handle
+                    .reply(Reply::new().embed(missing_permissions_embed(
+                        &self.ctx.bot.application.name,
+                        missing_permissions,
+                    )))
+                    .await?;
+
+                return Ok(());
+            }
+        }
+
         self.handle
             .reply(
                 Reply::new()
